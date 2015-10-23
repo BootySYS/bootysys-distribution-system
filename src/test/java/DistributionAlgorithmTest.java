b5e2b1ef24438ef8aa1e2a5ae60327ad10f0151e
jakob <jak.joachim@gmail.com>
2015-10-23 16:37:39 +0200

    Removed .gradle directory

---
+++ src/test/java/DistributionAlgorithmTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jakob on 23/10/15.
+ */
+public class DistributionAlgorithmTest {
+}
---
+++ src/test/java/tools/CourseTest.java
@@ -0,0 +1,7 @@
+package tools;
+
+/**
+ * Created by jakob on 23/10/15.
+ */
+public class CourseTest {
+}
